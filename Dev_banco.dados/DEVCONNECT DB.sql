8ac171741ea8fc17a074109a031c1f66f43c0251
Laura Anacleto <laur4kauany@gmail.com>
2025-10-02 16:19:50 -0300

    Realizado Scripts sobre DDL, DML E DQL da empresa DEVCONNECT

---
+++ Dev_banco.dados/DEVCONNECT DB.sql
@@ -0,0 +1,55 @@
+--DDL
+CREATE DATABASE db__devconnect;
+
+USE db__devconnect;
+
+CREATE TABLE tb_usuario (
+id INT IDENTITY(1,1) PRIMARY KEY,
+nome_completo        NVARCHAR (255)           NOT NULL, 
+nome_usuario         NVARCHAR (50)  UNIQUE    NOT NULL, 
+email                NVARCHAR (255) UNIQUE    NOT NULL, 
+senha                NVARCHAR (50)            NOT NULL, 
+foto_perfil_url      NVARCHAR (150)           NULL, 
+);
+SELECT * FROM tb_usuario;
+--------------------------------------------------------
+
+CREATE TABLE tb_publicacao (
+id INT IDENTITY(1,1) PRIMARY KEY,
+descricao            NVARCHAR (255)           NULL,
+imagem_url           NVARCHAR (150)           NULL,
+data_publicacao      DATE                     NOT NULL,
+
+id_usuario INT FOREIGN KEY REFERENCES tb_usuario(id)
+); 
+SELECT * FROM tb_publicacao;
+--------------------------------------------------------
+
+CREATE TABLE tb_seguidor (
+id_usuario_seguir INT NOT NULL,
+id_usuario_seguida INT NOT NULL,
+
+PRIMARY KEY (id_usuario_seguir, id_usuario_seguida)
+);
+SELECT * FROM tb_seguidor;
+--------------------------------------------------------
+
+CREATE TABLE tb_comentarios (
+id INT IDENTITY(1,1) PRIMARY KEY,
+texto                NVARCHAR (1000)       NOT NULL,
+data_comentario      DATE                  NOT NULL,
+
+id_usuario    INT FOREIGN KEY REFERENCES   tb_usuario(id),
+id_publicacao INT FOREIGN KEY REFERENCES   tb_publicacao(id)
+);
+SELECT * FROM tb_comentarios;
+--------------------------------------------------------
+
+CREATE TABLE tb_curtidas (
+id INT IDENTITY(1,1) PRIMARY KEY,
+
+id_usuario    INT FOREIGN KEY REFERENCES   tb_usuario(id),
+id_publicacao INT FOREIGN KEY REFERENCES   tb_publicacao(id)
+);
+SELECT * FROM tb_curtidas;
+
--- Dev_banco.dados/dql_DEVCONNECT.sql
+++
@@ -1,4 +0,0 @@
-USE db_devconnect;
-
-SELECT nome_completo, email
-FROM tb_usuario;
